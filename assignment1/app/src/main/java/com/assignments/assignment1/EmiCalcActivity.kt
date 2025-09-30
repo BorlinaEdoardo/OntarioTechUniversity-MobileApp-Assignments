0e3f74e71970c86756182e31f74a851d86188263
BorlinaEdoardo <edoardo.borlina@gmail.com>
2025-09-30 17:57:23 -0400

    feat(home layout): set basic layout and color palette for the home activity and Mortage calculator activity

---
+++ assignment1/app/src/main/java/com/assignments/assignment1/EmiCalcActivity.kt
@@ -0,0 +1,14 @@
+package com.assignments.assignment1
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class EmiCalcActivity : AppCompatActivity () {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_emi_calc)
+    }
+
+
+
+}
--- assignment1/app/src/main/java/com/assignments/assignment1/MainActivity.kt
+++ assignment1/app/src/main/java/com/assignments/assignment1/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.assignments.assignment1
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +18,13 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        // navigate to EmiCalcActivity on button click
+        findViewById<Button>(R.id.button3).setOnClickListener {
+            startActivity(Intent(this, EmiCalcActivity::class.java))
+        }
     }
+
+
+
 }
